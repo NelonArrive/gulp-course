c802e3d7a252a99031d33958fedbc9d517ed565e
NelonArrive <nelon.arrive@gmail.com>
2025-07-01 22:21:59 +0500

    💥 refactor code

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-const gulp = require('gulp')
+const gulp = require('gulp');
 
 // Tasks
-require('./gulp/dev.js')
-require('./gulp/docs.js')
+require('./gulp/dev.js');
+require('./gulp/docs.js');
 
 gulp.task(
 	'default',
@@ -11,7 +11,7 @@ gulp.task(
 		gulp.parallel('html:dev', 'sass:dev', 'images:dev', 'fonts:dev', 'files:dev', 'js:dev'),
 		gulp.parallel('server:dev', 'watch:dev')
 	)
-)
+);
 
 gulp.task(
 	'docs',
@@ -20,4 +20,4 @@ gulp.task(
 		gulp.parallel('html:docs', 'sass:docs', 'images:docs', 'fonts:docs', 'files:docs', 'js:docs'),
 		gulp.parallel('server:docs')
 	)
-)
+);
